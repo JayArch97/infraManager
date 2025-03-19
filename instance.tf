7e137b40cc63fb094df210e12a6b67526d02e5c3
Julio Armando Quintanilla Acosta <121356473+JayArch97@users.noreply.github.com>
2025-03-19 15:21:29 -0600

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "terraform" {
+  project      = "acostajulio-dev"
+  name         = "terraform"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
