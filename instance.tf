504ed5088e016fe50f847fd55685c3d1414a78b0
Julio Armando Quintanilla Acosta <121356473+JayArch97@users.noreply.github.com>
2025-03-19 15:32:22 -0600

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "terraform" {
   project      = "acostajulio-dev"
-  name         = "terraform"
+  name         = "terraform2"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
